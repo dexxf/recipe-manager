8e791a2f010e83e066f58d2895f4a69919e903a4
dexxf <Dexter101306@gmail.com>
2025-11-30 18:40:38 +0800

    feat(auth): add Permission enum for user privileges

---
+++ src/main/java/com/io/dexxf/domain/auth/valueobject/Permission.java
@@ -0,0 +1,13 @@
+package com.io.dexxf.domain.auth.valueobject;
+
+public enum Permission {
+    RECIPE_CREATE,
+    RECIPE_VIEW,
+    RECIPE_UPDATE,
+    RECIPE_DELETE,
+    RECIPE_SHARE,
+    RECIPE_SEARCH,
+    RECIPE_FAVORITE,
+    ADMIN_MANAGE_USERS,
+    ADMIN_MANAGE_RECIPES;
+}
